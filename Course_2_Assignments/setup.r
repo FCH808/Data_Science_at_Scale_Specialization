6262eb9029511789edb56c21e93568160ec13a57
Fernando Hernandez <fch808@gmail.com>
2016-03-07 04:39:44 +0900

    Add Course 2 R assignments; nothing pretty to look at but finds the answers needed

---
+++ Course_2_Assignments/setup.r
@@ -0,0 +1,12 @@
+
+install.packages("caret")
+install.packages("rpart")
+install.packages("tree")
+install.packages("randomForest")
+install.packages("e1071")
+install.packages("ggplot2")
+
+install.packages("dplyr")
+install.packages("readr")
+install.packages("rpart.plot")
+install.packages("rattle")
